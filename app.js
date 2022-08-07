16389eda5e20b1f5ffbb8956ed3bf60cc4fc97fc
Vincze Gábor <vinczegabor1981@gmail.com>
2022-08-07 10:09:57 +0200

    starter

---
+++ app.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectionString = "mongodb+srv://TBfan:MAxiKFMNaT6hokP0@nodetuts.odngg.mongodb.net/?retryWrites=true&w=majority"
+const connOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const init = async () => {
+    await mongoose.connect(connectionString, connOptions);
+    console.log("connected");
+};
+
+init();
